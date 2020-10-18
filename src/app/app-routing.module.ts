facbee67d90426460485d5bc6f7bb426b402aeda
Aymaru <aycastillo@ic-itcr.ac.cr>
2020-10-18 15:57:58 -0600

    Save and fetch data from server

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeInfoComponent } from './recipes/recipe-info/recipe-info.component';
+import { RecipesResolverService } from './recipes/recipes-resolver.service';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
@@ -13,8 +14,8 @@ const appRoutes: Routes = [
         children: [
             {path: '',component: RecipeInfoComponent, pathMatch: 'full'},
             {path: 'new', component: RecipeEditComponent},
-            {path: ':id', component: RecipeDetailComponent},
-            {path: ':id/edit', component: RecipeEditComponent}
+            {path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService]},
+            {path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService]}
         ]},
     {path: 'shopping-list', component: ShoppingListComponent}
 ];
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,30 +9,34 @@ export class RecipeService {
     
     recipesChanged = new Subject<Recipe[]>()
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            'Filet de pescado al ajillo',
-            'Delicioso filet de pescado al ajillo con ensalada para dos personas',
-            'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
-            [ new Ingredient ('Filet de pescado',2), 
-              new Ingredient ('Tomate', 1),
-              new Ingredient ('Lechuga', 2),
-              new Ingredient ('Aguacate', 1) ]
-        ),
-        new Recipe(
-            'Pizza de Pepperoni',
-            'Pizza de pepperoni con queso',
-            'https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_960_720.jpg',
-            [ new Ingredient ('Masa para pizza', 1),
-              new Ingredient ('Tomate', 4),
-              new Ingredient ('Queso', 1),
-              new Ingredient ('Pepperoni', 12)]
-        )
-    ];
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'Filet de pescado al ajillo',
+    //         'Delicioso filet de pescado al ajillo con ensalada para dos personas',
+    //         'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+    //         [ new Ingredient ('Filet de pescado',2), 
+    //           new Ingredient ('Tomate', 1),
+    //           new Ingredient ('Lechuga', 2),
+    //           new Ingredient ('Aguacate', 1) ]
+    //     ),
+    //     new Recipe(
+    //         'Pizza de Pepperoni',
+    //         'Pizza de pepperoni con queso',
+    //         'https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_960_720.jpg',
+    //         [ new Ingredient ('Masa para pizza', 1),
+    //           new Ingredient ('Tomate', 4),
+    //           new Ingredient ('Queso', 1),
+    //           new Ingredient ('Pepperoni', 12)]
+    //     )
+    // ];
+    private recipes: Recipe[] = [];
     
     constructor(private shoppingListService: ShoppingListService) {}
 
-
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.onChanged();
+    }
 
     getRecipes() {
         return this.recipes.slice();
